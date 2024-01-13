1793a3ef474010452f7a9d8fdaea57205bba8858
nc-mirandaa <n.miranda@correo.unimet.edu.ve>
2024-01-13 13:08:19 -0400

    Primer commit

---
+++ arepadinamicalista.py
@@ -0,0 +1,15 @@
+print("Vamos a hacer una arepa")
+variable= agua= input("Cuantas tazas de agua vas a usar: ")
+int(agua)
+variable= harina= input("Cuantas tazas de harina vas a usar: ")
+int(harina)
+variable= sal= input("Cuantas tazas de sal vas a usar: ")
+float(sal)
+variable= aceite= input("Cuantas tazas de aceite vas a usar: ")
+float(aceite)
+bowl= int(agua)+int(harina)+float(sal)
+bowl
+budare= float(aceite)+bowl
+budare
+print("Para hacer tu arepa necesitas", budare,"tazas")
+
